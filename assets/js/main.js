41e55624c52a2c9ed8dca4ba5098864bfeffdf17
PhongsakornP <i.phongsakornp@gmail.com>
2015-06-03 14:48:07 +0700

    Add supported browser warning message.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,40 @@
 jQuery(document).ready(function($) {
 
+  //http://stackoverflow.com/questions/5916900/how-can-you-detect-the-version-of-a-browser
+  function get_browser_info(){
+      var ua=navigator.userAgent,tem,M=ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || []; 
+      if(/trident/i.test(M[1])){
+          tem=/\brv[ :]+(\d+)/g.exec(ua) || []; 
+          return {name:'IE ',version:(tem[1]||'')};
+          }   
+      if(M[1]==='Chrome'){
+          tem=ua.match(/\bOPR\/(\d+)/)
+          if(tem!=null)   {return {name:'Opera', version:tem[1]};}
+          }   
+      M=M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+      if((tem=ua.match(/version\/(\d+)/i))!=null) {M.splice(1,1,tem[1]);}
+      return {
+        name: M[0],
+        version: M[1]
+      };
+   }
+
+  function check_supported_browser() {
+    var browser = get_browser_info();
+    var notChrome = browser.name != "Chrome";
+    var notSafari = browser.name != "Safari";
+
+    if(notChrome && notSafari) {
+
+      new jBox('Notice', {
+        content: $('#jNotify'),
+          autoClose: false
+      });
+    }
+  }
+
+  check_supported_browser();
+
   $('.tooltip').jBox('Tooltip', {
     position: {
         x: 'left',
